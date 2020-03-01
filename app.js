19b679af9877ad6b1cb5d2e930dee42bda348a40
Nano <gdanielgimenez@gmail.com>
2020-03-01 17:30:18 -0300

    updated index.html with ul  and appjs fecth user data works propperly

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 console.log('Hell on earth doom2');
+//jquery functions 
+$(document).ready(function(){
+    //get method with ajax
+    $.ajax({
+        method: 'GET',
+        url : 'https://jsonplaceholder.typicode.com/users',
+        dataType: 'json'
+    }).done(function(data){
+        //console.log(data);
+        //$.map(data, user =>$('#result').append( user.name+ '<br>'));
+        //print list of users and emails
+        //empty object to save the data we'll print
+        let userObj = {};
+        //new array of objects that contains the user name and his email
+        const userNamesEmails = $.map(data, user =>  userObj = {'name' :user.name, 'email': user.email });
+        //debug check via console
+        console.log(userNamesEmails);
+        //we print the array of users into an UL in the html
+        userNamesEmails.forEach(user => $('ul#data').append('<li> user : '+ user.name + ' Email : '+ user.email + '</li>')); 
+
+    });
+});
